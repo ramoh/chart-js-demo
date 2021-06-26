03a7094d0e21119a3bd9d45f8e1970619d88629c
ramoh <mohantyrajesh1984@gmail.com>
2021-06-26 23:42:43 +0530

    main page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,13 +7,17 @@
           Read the article on SitePoint:
           <a
             href="https://www.sitepoint.com/creating-beautiful-charts-vue-chart-js/"
+            target="_blank"
           >
             https://www.sitepoint.com/creating-beautiful-charts-vue-chart-js/
           </a>
         </h2>
         <h3>
           Download the repo from GitHub:
-          <a href="https://github.com/sitepoint-editors/vue-charts">
+          <a
+            href="https://github.com/sitepoint-editors/vue-charts"
+            target="_blank"
+          >
             https://github.com/sitepoint-editors/vue-charts
           </a>
         </h3>
