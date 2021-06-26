9e69a8a2e2abc516b2a84d255cb079d56864d45d
ramoh <mohantyrajesh1984@gmail.com>
2021-06-26 22:50:58 +0530

    jsconfig file

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,28 @@
 <template>
-  <div>
-    <h1>This is home page</h1>
-  </div>
+  <section class="hero">
+    <div class="hero-body">
+      <div class="container">
+        <h1>Creating Beautiful Charts Using Vue.js Wrappers For Chart.js</h1>
+        <h2>
+          Read the article on SitePoint:
+          <a
+            href="https://www.sitepoint.com/creating-beautiful-charts-vue-chart-js/"
+          >
+            https://www.sitepoint.com/creating-beautiful-charts-vue-chart-js/
+          </a>
+        </h2>
+        <h3>
+          Download the repo from GitHub:
+          <a href="https://github.com/sitepoint-editors/vue-charts">
+            https://github.com/sitepoint-editors/vue-charts
+          </a>
+        </h3>
+      </div>
+    </div>
+  </section>
 </template>
+<script>
+export default {
+  name: "home",
+};
+</script>
