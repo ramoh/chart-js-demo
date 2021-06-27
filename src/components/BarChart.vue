bbd3ceea102556205cdb968fc352062078e394cb
ramoh <mohantyrajesh1984@gmail.com>
2021-06-27 22:17:27 +0530

    bubble chart

--- src/components/BarChart.vue
+++ src/components/BarChart.vue
@@ -1,7 +1,7 @@
 <script>
 import { Bar } from "vue-chartjs";
 export default {
-  name: "bar",
+  name: "bar-chart",
   extends: Bar,
   data() {
     return {
--- src/views/VueChartJS.vue
+++ src/views/VueChartJS.vue
@@ -11,13 +11,23 @@
         <bar-chart></bar-chart>
       </div>
     </div>
+    <div class="columns">
+      <div class="column">
+        <h3>Bubble Chart</h3>
+        <bubble-chart></bubble-chart>
+      </div>
+      <div class="column">
+        <h3>Reactivity - Live update upon change in datasets</h3>
+      </div>
+    </div>
   </section>
 </template>
 <script>
 import LineChart from "../components/LineChart.vue";
 import BarChart from "../components/BarChart.vue";
+import BubbleChart from "../components/BubbleChart.vue";
 export default {
-  components: { LineChart, BarChart },
+  components: { LineChart, BarChart, BubbleChart },
   name: "vue-charts-js",
 };
 </script>
